748aa04baa9838b8b5ffc5ca0980c3b5f5b5b2a8
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-14 16:50:18 +0100

    3th commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Home } from "./pages/home";
 import { Dogs } from "./pages/dogs";
 import { Cats } from "./pages/cats";
+import { Header } from "./components/header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
     <Router>
+    <Header />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/cats" element={<Cats/>} />
